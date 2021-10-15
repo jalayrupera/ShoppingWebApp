0020207977853c70c165e40fda28e4a82283efd6
jalayrupera <jalayrupera13@gmail.com>
2021-10-15 10:24:52 +0530

    Activity Controller Added

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,38 @@
+﻿using API.Models;
+using API.Models.Database_Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            return await context.Activities.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+
+    }
+}
