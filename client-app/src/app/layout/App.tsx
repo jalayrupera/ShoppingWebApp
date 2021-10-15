dfca8f88cebe7595b2ed4b3ba60b09d77926f1ef
jalayrupera <jalayrupera13@gmail.com>
2021-10-15 18:34:14 +0530

    NavBar Added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
-import {Activity} from './models/activity';
+import {  Container, List } from 'semantic-ui-react';
+import {Activity} from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
 
@@ -14,8 +15,9 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <Header as ='h2' icon='users' content='reactivities' />
+    <>
+      <NavBar/>
+      <Container style={{marginTop: '7em' }}>
       <List>
       {activities.map((activity) => (
             <List.Item key={activity.id}>
@@ -23,7 +25,8 @@ function App() {
             </List.Item>
           ))}
       </List>
-    </div>
+      </Container>
+    </>
   );
 }
 
